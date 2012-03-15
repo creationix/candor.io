8babb7d1260aa43bd3a5ce80885406274aa49975
Tim Caswell <tim@creationix.com>
2012-03-14 19:48:02 -0500

    Fix Print to work for non null-terminated strings too

--- src/cio.cc
+++ src/cio.cc
@@ -2,6 +2,7 @@
 #include "candor.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 using namespace candor;
 
@@ -10,24 +11,32 @@ static Value* Print(uint32_t argc, Arguments& argv) {
   for (uint32_t i = 0; i < argc; i++) {
     Value* value = argv[i];
     const char* part;
+    uint32_t length;
     if (value->Is<Function>()) {
       part = "[Function]";
+      length = 10;
     } else if (value->Is<Object>()) {
       part = "[Object]";
+      length = 8;
     } else if (value->Is<Array>()) {
       part = "[Array]";
+      length = 7;
     } else if (value->Is<CData>()) {
       part = "[CData]";
+      length = 7;
     } else if (value->Is<Nil>()) {
       part = "nil";
+      length = 3;
     } else {
-      part = value->ToString()->Value();
+      String* string = value->ToString();
+      part = string->Value();
+      length = string->Length();
     }
 
     if (i == argc - 1) {
-      printf("%s\n", part);
+      printf("%.*s\n", length, part);
     } else {
-      printf("%s ", part);
+      printf("%.*s ", length, part);
     }
   }
   return Nil::New();
